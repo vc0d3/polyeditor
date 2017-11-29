c0add53df42cbfb8c199e66c6b6414cc2b0b240a
gv <renepfm@yahoo.com>
2017-11-28 22:51:19 -0600

    Start of Project

---
+++ Assets/Editor/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public static class ExtensionMethods
+    {
+        public static Vector3 ToXZ(this Vector3 v3)
+        {
+            return new Vector3(v3.x, v3.z);
+        }
+    }
+
---
+++ Assets/ShapeCreator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class ShapeCreator : MonoBehaviour
+    {
+
+        [HideInInspector]
+        public List<Shape> shapes = new List<Shape>();
+
+        public float HandleRadius = .5f;
+
+        
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+[System.Serializable]
+public class Shape
+{
+    public List<Vector3> points = new List<Vector3>();
+}
+
